31306e5b4ad247a4e88b0039912c486798247bd8
geebee22202 <90569659+geebee22202@users.noreply.github.com>
2022-06-25 22:47:12 -0400

    created functions to fetch api data and generate random quotes; manipulated DOM to display text from api

--- script.js
+++ script.js
@@ -1,2 +1,48 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
 
- 
+let apiQuotes = [];
+
+//Show new quote
+function newQuote() {
+  //Pick a random quote from API quotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+  //Check if author feild is blank and replace with 'unknown'
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  //check quote length to determine styling
+  if (quote.text.length > 90) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
+  quoteText.textContent = quote.text;
+}
+
+// Get quotes from API
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    //response value will not be set until the api has been retrieved; otherwise would cause error
+    const response = await fetch(apiUrl);
+    //getting json from api as a reponse and turning it into json object before passing to global variable: apiQuotes
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch error here
+  }
+}
+
+
+
+//On Load
+getQuotes();
